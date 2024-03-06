be41908b47f3c8d1f36ead3f14c0d9e1cb59a830
pankaj143s <pankajsarnobat143@gmail.com>
2024-03-07 00:22:39 +0530

    Refine button hover effect on product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -43,13 +43,15 @@ const Product = () => {
             selectedProduct && selectedProduct.id === product.id;
           return (
             <button
-              className={`relative h-12 w-40 overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80 ${
-                isSelected ? "border-2 border-primary" : ""
+              className={`relative h-12 w-40 overflow-hidden border border-primary text-primary shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 rounded-full before:rounded-full before:bg-primary before:duration-300 before:ease-out hover:text-white hover:shadow-primary hover:before:h-40 hover:before:w-40 hover:before:opacity-80 ${
+                isSelected ? " bg-primary text-white" : ""
               }`}
               key={product.id}
               onClick={() => handleProductClick(product)}
             >
-              <span className="relative z-10 font-poppins">{product.name}</span>
+              <span className="relative z-10 font-poppins font-semibold">
+                {product.name}
+              </span>
             </button>
           );
         })}
