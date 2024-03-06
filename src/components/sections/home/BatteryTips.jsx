79d334bed6a808c3babf48533ed61c905fa44e4d
pankaj143s <pankajsarnobat143@gmail.com>
2024-03-06 16:44:10 +0530

    added scroll to reveal in homepage

--- src/components/sections/home/BatteryTips.jsx
+++ src/components/sections/home/BatteryTips.jsx
@@ -1,56 +1,71 @@
 import React from "react";
 import batteryCharge from "../../../assets/battry-charge.png";
 import { Link } from "react-router-dom";
+import { Fade } from "react-awesome-reveal";
 const BatteryTips = () => {
   return (
     <div>
       <section className="text-black font-poppins px-4">
         <div className="container px-5 mx-auto">
-          <div className="text-center py-4">
-            <h1 className="sm:text-4xl text-4xl font-semibold   text-primary py-8 ">
-              Battery Tips
-            </h1>
-            <p className="text-xl leading-relaxed mx-auto font-semibold">
-              Know how to take care of your battery
-            </p>
-          </div>
+          <Fade direction="up">
+            <div className="text-center py-4">
+              <h1 className="sm:text-4xl text-4xl font-semibold   text-primary py-8 ">
+                Battery Tips
+              </h1>
+              <p className="text-xl leading-relaxed mx-auto font-semibold">
+                Know how to take care of your battery
+              </p>
+            </div>
+          </Fade>
           <div className="flex p-5 flex-wrap justify-center sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 md:gap-y-5 space-y-6 md:space-x-4">
             <div className="p-4 w-full md:w-1/3 space-y-7 lg:w-1/4  flex flex-col text-center items-center hover:bg-black hover:delay-200 duration-1000 ease-out hover:text-white cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
               <Link to="/faq">
-                <div className=" inline-flex items-center justify-center rounded-full  flex-shrink-0 ">
-                  <img src={batteryCharge} alt="" className="w-32" />
-                </div>
-                <div className="flex-grow">
-                  <p className=" text-lg ">
-                    How do i know that my battery needs replacement?
-                  </p>
-                </div>
+                <Fade direction="up">
+                  <div className=" inline-flex items-center justify-center rounded-full  flex-shrink-0 ">
+                    <img src={batteryCharge} alt="" className="w-32" />
+                  </div>
+                </Fade>
+                <Fade direction="up">
+                  <div className="flex-grow">
+                    <p className=" text-lg ">
+                      How do i know that my battery needs replacement?
+                    </p>
+                  </div>
+                </Fade>
               </Link>
             </div>
 
             <div className="p-4 w-full md:w-1/3 space-y-7 lg:w-1/4  flex flex-col text-center items-center hover:bg-black hover:delay-200 duration-1000 ease-out hover:text-white cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
               <Link to="/faq">
-                <div className=" inline-flex items-center justify-center rounded-full  flex-shrink-0 ">
-                  <img src={batteryCharge} alt="" className="w-32" />
-                </div>
-                <div className="flex-grow">
-                  <p className=" text-lg ">
-                    Tips while replacing the battery or cleaning terminals
-                  </p>
-                </div>
+                <Fade direction="up">
+                  <div className=" inline-flex items-center justify-center rounded-full  flex-shrink-0 ">
+                    <img src={batteryCharge} alt="" className="w-32" />
+                  </div>
+                </Fade>
+                <Fade direction="up">
+                  <div className="flex-grow">
+                    <p className=" text-lg ">
+                      Tips while replacing the battery or cleaning terminals
+                    </p>
+                  </div>
+                </Fade>
               </Link>
             </div>
             <div className="p-4 w-full md:w-1/3 space-y-7 lg:w-1/4  flex flex-col text-center items-center hover:bg-black hover:delay-200 duration-1000 ease-out hover:text-white cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
               <Link to="/faq">
-                <div className=" inline-flex items-center justify-center rounded-full  flex-shrink-0 ">
-                  <img src={batteryCharge} alt="" className="w-32" />
-                </div>
-                <div className="flex-grow">
-                  <p className=" text-lg  ">
-                    Battery Recycling- how to handle and used batteries with
-                    care
-                  </p>
-                </div>
+                <Fade direction="up">
+                  <div className=" inline-flex items-center justify-center rounded-full  flex-shrink-0 ">
+                    <img src={batteryCharge} alt="" className="w-32" />
+                  </div>
+                </Fade>
+                <Fade direction="up">
+                  <div className="flex-grow">
+                    <p className=" text-lg  ">
+                      Battery Recycling- how to handle and used batteries with
+                      care
+                    </p>
+                  </div>
+                </Fade>
               </Link>
             </div>
           </div>
--- src/components/sections/home/LifeAtEmiko.jsx
+++ src/components/sections/home/LifeAtEmiko.jsx
@@ -1,25 +1,31 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 
 const LifeAtEmiko = () => {
   return (
     <>
       <div className="font-poppins  flex flex-col md:flex-row justify-center mx-auto">
         <div className="md:w-1/2 flex  bg-primary items-center justify-center text-white md:text-center text-center font-poppins font-semibold text-2xl p-4 md:p-4">
-          <h3 className="">Great Place To Learn</h3>
+          <Fade direction="up">
+            <h3 className="">Great Place To Learn</h3>
+          </Fade>
         </div>
 
         <div className="bg-slate-200 bg-gradient-to-l from-white to-blue-500 py-4  px-4">
-          <h1 className="flex md:justify-center text-left md:text-center text-4xl font-semibold   text-primary">
-            Life At Emiko
-          </h1>
-          <p className="flex justify-center text-xl leading-7 md:px-24 text-left md:text-center md:pb-2">
-            Embrace a dynamic work culture that fosters creativity,
-            collaboration, and innovation. We prioritize diversity, encourage
-            continuous learning, and value work-life balance. Join a team-driven
-            environment where every voice is heard, ideas are celebrated, and
-            excellence is recognized. Elevate your career in a supportive and
-            inclusive workplace.
-          </p>
+          <Fade direction="up">
+            <h1 className="flex md:justify-center text-left md:text-center text-4xl font-semibold   text-primary">
+              Life At Emiko
+            </h1>
+
+            <p className="flex justify-center text-xl leading-7 md:px-24 text-left md:text-center md:pb-2">
+              Embrace a dynamic work culture that fosters creativity,
+              collaboration, and innovation. We prioritize diversity, encourage
+              continuous learning, and value work-life balance. Join a
+              team-driven environment where every voice is heard, ideas are
+              celebrated, and excellence is recognized. Elevate your career in a
+              supportive and inclusive workplace.
+            </p>
+          </Fade>
         </div>
       </div>
     </>
